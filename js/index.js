188c253c9d3d8e5bfd0cfb6e977e3c7a96a00175
Jian-Min-Huang <yfr.huang@hotmail.com>
2017-08-17 23:27:19 +0800

    refactor

--- js/index.js
+++ js/index.js
@@ -1,12 +1,11 @@
-
-$(document).ready(function() {
-    var example = "238新北市樹林區中山路二段34號,1\n238新北市樹林區中山路二段128號,2";
+$(document).ready(function () {
+    var example = "黃OO,238新北市樹林區中山路二段34號,1\n陳XX,238新北市樹林區中山路二段128號,2\n李YY,238新北市樹林區中山路二段150號,3";
     $("#data").attr("placeholder", example);
 
-    initMap();
+    initMap(0);
 });
 
-function initMap() {
+function initMap(type) {
     var center = new google.maps.LatLng(24.983952, 121.414933);
     var mapProp = {
         center: center,
@@ -15,40 +14,44 @@ function initMap() {
     };
     var map = new google.maps.Map(document.getElementById("map"), mapProp);
 
-    var marker = createMarker(center, "./img/star.png", "google.maps.Animation.BOUNCE");
+    var marker = (type === 0) ? createMarker(center, "./img/star.png", null) : createMarker(center, "./img/star.png", "google.maps.Animation.BOUNCE");
     marker.setMap(map);
+
+    var infowindow = new google.maps.InfoWindow({content: "<b>衛生所</b>"});
+    if (type === 0) infowindow.open(map, marker);
+
+    return map;
 }
 
 function process() {
-    $("map").empty();
+    $("#map").empty();
+    $("#check").empty();
 
     var lines = $("#data").val().split("\n");
 
     var data = [];
     lines.forEach(function (element) {
-        var address_text = element.split(",");
-        data.push({location: address2latlng(address_text[0]), note: address_text[1]});
+        var name_address_level = element.split(",");
+        data.push({name: name_address_level[0], location: address2latlng(name_address_level[1]), level: name_address_level[2]});
     });
 
-    // init map
-    var center = new google.maps.LatLng(24.983952, 121.414933);
-    var mapProp = {
-        center: center,
-        zoom: 13,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map"), mapProp);
+    var map = initMap(1);
 
-    var marker = createMarker(center, "./img/star.png", "google.maps.Animation.BOUNCE");
-    marker.setMap(map);
-
-    data.forEach(function(element){
+    var html = "";
+    data.forEach(function (element) {
         if (element.location === null) return;
 
         var latlng = new google.maps.LatLng(element.location.lat, element.location.lng);
-        var marker = createMarker(latlng, null, null);
+        var marker = createMarker(latlng, element.level, null);
         marker.setMap(map);
+
+        var infowindow = new google.maps.InfoWindow({content: element.name});
+        infowindow.open(map, marker);
+
+        html += "<input type=\"checkbox\" checked />" + element.name + "<br>";
     });
+
+    $("#check").append(html);
 }
 
 function address2latlng(address) {
@@ -75,11 +78,24 @@ function address2latlng(address) {
 
 function createMarker(position, iconPath, animationType) {
     var marker = new google.maps.Marker({
-        position: position,
+        position: position
     });
 
     if (iconPath !== null) {
-        marker.setIcon(iconPath);
+        switch (iconPath) {
+            case "1":
+                marker.setIcon("./img/1.png");
+                break;
+            case "2":
+                marker.setIcon("./img/2.png");
+                break;
+            case "3":
+                marker.setIcon("./img/3.png");
+                break;
+            default:
+                marker.setIcon(iconPath);
+                break;
+        }
     }
     if (animationType !== null) {
         marker.setAnimation(eval(animationType));
@@ -87,3 +103,6 @@ function createMarker(position, iconPath, animationType) {
 
     return marker;
 }
+
+
+
