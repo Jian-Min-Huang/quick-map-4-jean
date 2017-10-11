5771686b5670771dfe7bdbda698785eb9965b5e2
Jian-Min-Huang <yfr.huang@hotmail.com>
2017-10-11 20:09:04 +0800

    refactor

--- js/index_m.js
+++ js/index_m.js
@@ -3,9 +3,6 @@ $(document).ready(function () {
          window.location = "./index.html";
     }
 
-    // var example = "姓名,地址,等級\n黃OO,238新北市樹林區中山路二段34號,1\n陳XX,238新北市樹林區中山路二段128號,2\n李YY,238新北市樹林區中山路二段150號,3";
-    // $("#data").attr("placeholder", example);
-
     initMap(0);
 });
 
@@ -13,16 +10,20 @@ var map = null;
 var markers = [];
 var infoWindows = [];
 
+/**
+ * type 0 when page init
+ * type 1 when process
+ */
 function initMap(type) {
-    var office = new google.maps.LatLng(24.983952, 121.414933);
+    var officeLocation = new google.maps.LatLng(24.983952, 121.414933);
     var mapProp = {
-        center: office,
+        center: officeLocation,
         zoom: 14,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     var map = new google.maps.Map(document.getElementById("map"), mapProp);
 
-    var marker = (type === 0) ? createMarker(office, "./img/star.png", null) : createMarker(office, "./img/star.png", "google.maps.Animation.BOUNCE");
+    var marker = (type === 0) ? createMarker(officeLocation, "./img/star.png", null) : createMarker(officeLocation, "./img/star.png", "google.maps.Animation.BOUNCE");
     marker.setMap(map);
 
     var infowindow = new google.maps.InfoWindow({content: "<b>衛生所</b>"});
@@ -36,7 +37,9 @@ function process() {
     infoWindows = [];
     $("#map").empty();
     $("#check").empty();
+    $("#error").empty();
 
+    /* read data from text area */
     var lines = null;
     if ($("#data").val() === "") {
         lines = $("#data").attr("placeholder").split("\n");
@@ -46,11 +49,18 @@ function process() {
     }
 
     var data = [];
+    var errAddr = [];
     lines.forEach(function (element) {
         var name_address_level = element.split(",");
+        var tmpLocation = address2latlng(name_address_level[2]);
+
+        if (tmpLocation.lat === 24.983952 && tmpLocation.lng === 121.414933) {
+            errAddr.push(element);
+        }
+
         data.push({
             name: name_address_level[0],
-            location: address2latlng(name_address_level[2]),
+            location: tmpLocation,
             level: name_address_level[1]
         });
     });
@@ -73,37 +83,44 @@ function process() {
         html += "<span><input id=\"idx" + i + "\" type=\"checkbox\" onclick=\"showAndHideMarker(" + i + ")\" checked>" + data[i].name + "</span>";
     }
 
+    for (var i = 0; i < errAddr.length; i++) {
+        if (i === 0) errHtml.append("==地址解析錯誤 請修正原始檔==");
+
+        errHtml += `${errAddr[i]}<br>`;
+    }
+
     $("#check").append(html);
+    $("#error").append(errHtml);
 }
 
 function address2latlng(address) {
-    var result = null;
+    var latlng = null;
 
     $.ajax({
-        url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyAj4PdVqJ5dptTNojTHop1tUsird2yxZgg",
+        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyAj4PdVqJ5dptTNojTHop1tUsird2yxZgg`,
         method: "GET",
         async: false,
         success: function (res) {
-            if (res.results[0].geometry.location) {
-                result = {lat: res.results[0].geometry.location.lat, lng: res.results[0].geometry.location.lng};
+            if (res.results.length > 0) {
+                latlng = {lat: res.results[0].geometry.location.lat, lng: res.results[0].geometry.location.lng};
             } else {
-                console.log(address + "解析錯誤 : " + res);
+                latlng = {lat: 24.983952, lng: 121.414933};
+                console.error(`${address} parsing error : ${res}`);
             }
         },
         error: function (xhr, status, error) {
-            console.log(JSON.stringify(xhr) + ",\n" + status + ",\n" + error);
+            console.error(`${JSON.stringify(xhr)},\n${status},\n${error}`);
         }
     });
 
-    return result;
+    return latlng;
 }
 
 function createMarker(position, iconPath, animationType) {
-    var marker = new google.maps.Marker({
-        position: position
-    });
+    var marker = new google.maps.Marker();
 
-    if (iconPath !== null) {
+    if (position) marker.setPosition(position);
+    if (iconPath) {
         switch (iconPath) {
             case "一級":
                 marker.setIcon("./img/1.png");
@@ -114,12 +131,15 @@ function createMarker(position, iconPath, animationType) {
             case "三級":
                 marker.setIcon("./img/3.png");
                 break;
+            case "四級":
+                marker.setIcon("./img/4.png");
+                break;
             default:
-                marker.setIcon("./img/3.png");
+                marker.setIcon(iconPath);
                 break;
         }
     }
-    if (animationType !== null) {
+    if (animationType) {
         marker.setAnimation(eval(animationType));
     }
 
